9e0024453626e3a5606579150c2b04139634c164
50773sa <93046590+50773sa@users.noreply.github.com>
2024-08-21 14:48:12 +0200

    Add the `innerWrapper` component and decrease the size of the icons

--- src/components/header/children/TopBar.jsx
+++ src/components/header/children/TopBar.jsx
@@ -1,3 +1,5 @@
+// components
+import InnerWrapper from '../../wrappers/InnerWrapper'
 // mui
 import Box from '@mui/material/Box'
 import GitHubIcon from '@mui/icons-material/GitHub'
@@ -6,25 +8,27 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn'
 const TopBar = ({ theme }) => {
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', bgcolor: theme.palette.color.orange.dark, px: 1, py: 0.5 }} >
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%', maxWidth: 1200 }}>
-                <a 
-                    aria-label='go to my linkedIn page' 
-                    href='https://www.linkedin.com/in/sara-almqvist-93862774/' 
-                    target='_blank' 
-                    rel='noopener noreferrer'
-                >
-                    <LinkedInIcon sx={{ ml: 1 }} />
-                </a>
-                <a 
-                    href='https://github.com/50773sa'
-                    aria-label='go to my github page'
-                    target='_blank' 
-                    rel='noopener noreferrer'
-                >
-                    <GitHubIcon sx={{ ml: 1 }} />  
-                </a>
-            </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', bgcolor: theme.palette.color.orange.dark, py: 0.5 }} >
+            <InnerWrapper>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+                    <a 
+                        aria-label='go to my linkedIn page' 
+                        href='https://www.linkedin.com/in/sara-almqvist-93862774/' 
+                        target='_blank' 
+                        rel='noopener noreferrer'
+                    >
+                        <LinkedInIcon sx={{ ml: 1, fontSize: 'large' }} />
+                    </a>
+                    <a 
+                        href='https://github.com/50773sa'
+                        aria-label='go to my github page'
+                        target='_blank' 
+                        rel='noopener noreferrer'
+                    >
+                        <GitHubIcon sx={{ ml: 1, fontSize: 'large' }} />   
+                    </a>
+                </Box>
+            </InnerWrapper>
         </Box>
     )
 }
