ebb6960de74cb5c2fde1772c04abb81b3480f579
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-01 12:46:47 +0200

    Add the page title

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,35 @@
+import heroTitle from '../../src/assets/heroTitle.svg'
+// mui
+import Box from '@mui/material/Box'
 
 const Homepage = () => {
+
     return (
-        <div>Homepage</div>
+        <Box 
+            sx={{ 
+                display: 'flex', 
+                flexDirection: 'column', 
+                justifyContent: 'center',
+                height: '100vh', 
+                width: 'auto',
+            }}
+        >
+            <Box 
+                sx={{ 
+                    display: 'flex', 
+                    alightItems: 'center', 
+                    width: {xs: '100%', sm: '75%', md: '50%'}, 
+                    px: {xs: 4, md: 10}, 
+                }}
+            >
+                <img 
+                    src={heroTitle} 
+                    alt='Page title: Sara Almqvist, Frontend Developer' 
+                    width='100%'
+                    height='100%'
+                />
+            </Box>
+        </Box>
     )
 }
 
