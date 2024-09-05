ac50788236f1eaf485a4b3b08ec828c3fb9fa8bd
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-05 16:28:07 +0200

    Create a section for 'About' including lorem impsum text

---
+++ src/components/home/AboutSection.jsx
@@ -0,0 +1,21 @@
+// components
+import SectionWrapper from './SectionWrapper'
+import TitleWithDescription from './TitleWithDescription'
+
+const AboutSection = () => {
+    return (
+        <SectionWrapper bgcolor='light'>
+            <TitleWithDescription
+                 h2='About' 
+                 paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,'
+            />
+        </SectionWrapper>
+    )
+}
+
+export default AboutSection
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,12 @@
 // components
+import AboutSection from "../components/home/AboutSection"
 import Hero from "../components/home/hero/Hero"
 
-
 const Homepage = () => {
-
     return (  
         <>
             <Hero />
+            <AboutSection />
         </>
     )
 }
