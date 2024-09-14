88cb5de2667ab6c31f7b9038ea1913aef6395577
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-14 19:12:38 +0200

    Create a content-free card for the 'Tech Stack'

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 // components
-import AboutSection from "../components/home/AboutSection"
+import AboutSection from "../components/home/about/AboutSection"
 import Hero from "../components/home/hero/Hero"
+import TechStack from "../components/home/techStack/TechStack"
 
 const Homepage = () => {
     return (  
         <>
             <Hero />
             <AboutSection />
+            <TechStack />
         </>
     )
 }
