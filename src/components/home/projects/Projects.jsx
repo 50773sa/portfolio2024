ca18ce6a98ef98b6fce5f1bd182cf76abed9468d
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-14 19:24:54 +0200

    Create a basic section for the projects

---
+++ src/components/home/projects/Projects.jsx
@@ -0,0 +1,16 @@
+// components
+import SectionWrapper from "../SectionWrapper"
+import TitleWithDescription from "../TitleWithDescription"
+
+const Projects = () => {
+    return (
+        <SectionWrapper bgcolor='light'>
+            <TitleWithDescription 
+                h2='Projects'
+                paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae '
+            />
+        </SectionWrapper>
+    )
+}
+
+export default Projects
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 // components
 import AboutSection from "../components/home/about/AboutSection"
 import Hero from "../components/home/hero/Hero"
+import Projects from "../components/home/projects/Projects"
 import TechStack from "../components/home/techStack/TechStack"
 
 const Homepage = () => {
@@ -9,6 +10,7 @@ const Homepage = () => {
             <Hero />
             <AboutSection />
             <TechStack />
+            <Projects />
         </>
     )
 }
