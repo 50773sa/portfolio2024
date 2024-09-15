1b6f38d7bba4c6308378910ea7a6509ac96c2174
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-15 13:15:13 +0200

    Show overlay on hover

--- src/components/home/projects/PlaceholderCard.jsx
+++ src/components/home/projects/PlaceholderCard.jsx
@@ -12,18 +12,34 @@ const PlaceholderCard = ({ image }) => {
         width: '300px',
         cursor: 'default',
         background: '#636363',
+        '&:hover div': {
+            display: 'block',
+        },
+        position: 'relative',
+    }))
+
+    const OverlayDiv = styled('div')(() => ({
+        display: 'none', 
+        backgroundColor: '#00000090',
+        inset: 0,
+        position: 'absolute',
     }))
 
     return (
         <Grid container gap={4} justifyContent='center'>
-            {[...Array(6)].map(() => (
-                <StyledCard>
+            {[...Array(6)].map((i) => (
+                <StyledCard key={i}>
                     <CardContent sx={{ '&:last-child': { padding: 0 } }}>
                         {!image
                             ?   <Skeleton variant="rectangle" width={300} height={200} />
                             :   <img src={image} alt='' width='100%' height='100%' />
                         }
                     </CardContent>
+                    
+                    <OverlayDiv>
+
+                    </OverlayDiv> 
+
                 </StyledCard>  
             ))}
         </Grid> 
