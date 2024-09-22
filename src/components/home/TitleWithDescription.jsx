d2fbae1f71ebd0125c370f3dfbdb34212f932512
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-22 15:11:08 +0200

    Styling: Set cursor to default

--- src/components/home/TitleWithDescription.jsx
+++ src/components/home/TitleWithDescription.jsx
@@ -4,11 +4,11 @@ import Typography from '@mui/material/Typography'
 const TitleWithDescription = ({ h2, paragraph }) => {
     return (
         <>
-            <Typography component='h2' variant='h4' sx={{ textAlign: 'center', mb: 2 }}>
+            <Typography component='h2' variant='h4' sx={{ textAlign: 'center', mb: 2, cursor: 'default' }}>
                 { h2 }
             </Typography>
 
-            <Typography component='p' variant='subtitle1' align='center' mb={5}>
+            <Typography component='p' variant='subtitle1' align='center' mb={5} sx={{ cursor: 'default' }}>
                 { paragraph }
             </Typography>
         </>
