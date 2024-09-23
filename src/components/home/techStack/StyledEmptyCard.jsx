bc55e335f84343a8b3fa9f907d5fb8c2c1281d79
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-23 10:06:40 +0200

    Remove fixed width and margin

--- src/components/home/techStack/StyledEmptyCard.jsx
+++ src/components/home/techStack/StyledEmptyCard.jsx
@@ -15,9 +15,7 @@ const StyledEmptyCard = ({ logo, name }) => {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        height: '200px',
-        width: '150px',
-        margin: 4,
+        minHeight: '200px',
         cursor: 'default',
         borderRadius: '40px',
         background: 'linear-gradient(180deg, #636363, #C9C9C9)',
@@ -35,28 +33,30 @@ const StyledEmptyCard = ({ logo, name }) => {
     }))
 
     return (
-        <Grid container gap={4} justifyContent='center' px={{ xs: 0, md: 10 }}>
-            {[...Array(11)].map(() => (
-                <StyledCard>
-                    <StyledCardContent>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: -10, flexBasis: '80%' }}>
-                            {!logo
-                                ?   <Skeleton variant="circular" width={100} height={100} />
-                                :   <img  src='#' alt='' width='100%' height='100%' />
-                            }
-                        </div>
-                        <Typography 
-                            sx={{ 
-                                display: 'flex', 
-                                justifyContent: 'center', 
-                                flexBasis: '20%', 
-                                color: theme.palette.color.black 
-                            }}
-                        >
-                            {name}
-                        </Typography>
-                    </StyledCardContent>
-                </StyledCard>  
+        <Grid container spacing={4} justifyContent='center'>
+            {[...Array(11)].map((i) => (
+                <Grid xs={6} sm={4} md={3} key={i}>
+                    <StyledCard>
+                        <StyledCardContent>
+                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: -10, flexBasis: '80%' }}>
+                                {!logo
+                                    ?   <Skeleton variant="circular" width={100} height={100} />
+                                    :   <img  src='#' alt='' width='100%' height='100%' />
+                                }
+                            </div>
+                            <Typography 
+                                sx={{ 
+                                    display: 'flex', 
+                                    justifyContent: 'center', 
+                                    flexBasis: '20%', 
+                                    color: theme.palette.color.black 
+                                }}
+                            >
+                                {name}
+                            </Typography>
+                        </StyledCardContent>
+                    </StyledCard>  
+                </Grid>
             ))}
         </Grid> 
     )
