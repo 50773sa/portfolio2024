456972c545239c529b6f315ee15973c9eb3cda19
50773sa <93046590+50773sa@users.noreply.github.com>
2024-09-29 15:24:23 +0200

    Move the skeleton to its own component

---
+++ src/components/home/projects/children/LoadingImgSkeletons.jsx
@@ -0,0 +1,24 @@
+// mui
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import Skeleton from '@mui/material/Skeleton'
+
+const LoadingImgSkeletons = () => {
+    return (
+        <Grid container spacing={4} justifyContent='center' mb={4} px={2}>
+            {[...Array(6)].map((i) => (
+                <Grid xs={12} sm={6} md={4}>
+                    <Skeleton 
+                        key={i} 
+                        variant="rectangle" 
+                        sx={{ 
+                            minWidth: '200px', 
+                            minHeight: '175px',
+                        }}
+                    />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
+
+export default LoadingImgSkeletons
