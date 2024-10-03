b18a4c068c331e47ee2ed9ae9c4f33fe433f71b1
50773sa <93046590+50773sa@users.noreply.github.com>
2024-10-03 12:56:02 +0200

    Import data from the server and remove the 'dummy'-data

--- src/components/home/techStack/StyledEmptyCard.jsx
+++ src/components/home/techStack/StyledEmptyCard.jsx
@@ -3,22 +3,19 @@ import { useTheme } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import Grid from '@mui/material/Unstable_Grid2/Grid2'
-import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
 
 
-const StyledEmptyCard = ({ logo, name }) => {
+const StyledEmptyCard = ({ image, name }) => {
     const theme = useTheme()
 
     const StyledCard = styled(Card)(() => ({
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        // minHeight: '200px',
-        aspectRatio: '4 / 4',
+        aspectRatio: '4/4',
         cursor: 'default',
-        borderRadius: '40px',
+        borderRadius: '20px',
         background: 'linear-gradient(180deg, #636363, #C9C9C9)',
         boxShadow: 'rgba(255, 255, 255, 0.24) 0px 3px 8px',
     }))
@@ -34,32 +31,24 @@ const StyledEmptyCard = ({ logo, name }) => {
     }))
 
     return (
-        <Grid container spacing={4} justifyContent='center'>
-            {[...Array(11)].map((i) => (
-                <Grid xs={6} sm={4} md={3} key={i}>
-                    <StyledCard>
-                        <StyledCardContent>
-                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: -10, flexBasis: '80%' }}>
-                                {!logo
-                                    ?   <Skeleton variant="circular" width='70%' height='80%' />
-                                    :   <img  src='#' alt='' width='100%' height='100%' />
-                                }
-                            </div>
-                            <Typography 
-                                sx={{ 
-                                    display: 'flex', 
-                                    justifyContent: 'center', 
-                                    flexBasis: '20%',
-                                    color: theme.palette.color.black 
-                                }}
-                            >
-                                {name ? name : 'Name'}
-                            </Typography>
-                        </StyledCardContent>
-                    </StyledCard>  
-                </Grid>
-            ))}
-        </Grid> 
+        <StyledCard>
+            <StyledCardContent>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: -10, flexBasis: '80%' }}>
+                    <img src={image} alt='' width='50%' height='auto' />
+                </div>
+                <Typography 
+                    sx={{ 
+                        display: 'flex', 
+                        justifyContent: 'center', 
+                        alignItems: 'center',
+                        flexBasis: '20%',
+                        color: theme.palette.color.black 
+                    }}
+                >
+                    {name}
+                </Typography>
+            </StyledCardContent>
+        </StyledCard>  
     )
 }
 
--- src/components/home/techStack/TechStack.jsx
+++ src/components/home/techStack/TechStack.jsx
@@ -1,17 +1,31 @@
 // components
 import SectionWrapper from "../SectionWrapper"
 import StyledEmptyCard from "./StyledEmptyCard"
+import TechStackImgSkeleton from "./TechStackImgSkeleton"
 import TitleWithDescription from "../TitleWithDescription"
+// mui
+import Grid from '@mui/material/Unstable_Grid2/Grid2'
 
-const TechStack = () => {
+const TechStack = ({ techStack, isLoading }) => {
     return (
         <SectionWrapper>
             <TitleWithDescription 
                 h2='Current Tech Stack'
                 paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae '
             />
+            <Grid container spacing={4} justifyContent='center'>
 
-            <StyledEmptyCard />
+                {isLoading && <TechStackImgSkeleton />}
+
+                {!isLoading && techStack.map((tech) => (
+                    <Grid xs={6} sm={4} md={3} key={tech.name}>    
+                        <StyledEmptyCard 
+                            image={tech.logoPath}
+                            name={tech.name}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
         </SectionWrapper>
     )
 }
