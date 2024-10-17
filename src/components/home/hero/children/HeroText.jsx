fe201c24d97d2e5c9251d185b4595790387413ea
50773sa <93046590+50773sa@users.noreply.github.com>
2024-10-17 10:11:11 +0200

    Styling: Increase 'fontWeight' and set 'whiteSpace' to 'noWrap'

--- src/components/home/hero/children/HeroText.jsx
+++ src/components/home/hero/children/HeroText.jsx
@@ -4,12 +4,15 @@ import Typography from '@mui/material/Typography'
 const HeroText = ({ theme }) => {
     return (
         <>
-            <Typography component='h1' sx={{ display: 'flex', flexDirection: 'column', textTransform: 'uppercase' }}>
+            <Typography 
+                component='h1' 
+                sx={{ display: 'flex', flexDirection: 'column', textTransform: 'uppercase' }}
+            >
                 <Typography 
                     component='span' 
                     sx={{ 
                         fontSize: { xs: '1.5rem', sm: '2.5rem', md: '2.5rem' }, 
-                        mb: {xs: -2, sm: -3} 
+                        mb: {xs: -2, sm: -3},
                     }}
                 >
                     Sara 
@@ -18,7 +21,7 @@ const HeroText = ({ theme }) => {
                     component='span' 
                     sx={{ 
                         fontSize: { xs: '3rem', sm: '4.5rem', md: '5rem' }, 
-                        fontWeight: 600 
+                        fontWeight: 800 
                     }}
                 >
                     Almqvist
@@ -29,13 +32,15 @@ const HeroText = ({ theme }) => {
                 component='h2' 
                 sx={{ 
                     fontSize: { xs: '0.8rem', sm: '1.2rem', md: '1.3rem' }, 
-                    mb: -2 
+                    mb: -2 ,
+                    whiteSpace: 'nowrap',
                 }}
             >
                 <Typography 
                     component='span' 
                     variant='' 
-                    sx={{ color: theme.palette.color.orange.light 
+                    sx={{ 
+                        color: theme.palette.color.orange.light 
                     }}
                 >
                     Front
