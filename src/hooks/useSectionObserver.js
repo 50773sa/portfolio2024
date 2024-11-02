f00ff4d57d522aa08de2e2394a0abd2532b3e743
50773sa <93046590+50773sa@users.noreply.github.com>
2024-11-02 17:21:43 +0100

    feat: Implement section observer to update URL on scroll
    - Added functionality to observe sections and update the URL based on visibility.
    - Improved navigation experience by allowing seamless scrolling between sections.
    - Ensured home section resets URL when scrolled to the top.

---
+++ src/hooks/useSectionObserver.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+
+const useSectionObserver = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const sections = document.querySelectorAll('[datasectionid]')
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    const sectionId = entry.target.getAttribute('datasectionid')
+
+                    if (entry.isIntersecting) {
+
+                        // Update the URL with the section ID
+                        if (sectionId !== 'home') {
+                            navigate(sectionId, { replace: true })
+                        } 
+                        if(sectionId === 'home') {
+                            navigate('/', { replace: true })
+                        }
+                    } 
+                })
+            },
+            { threshold: 1 } // Observe when section is in view
+        )
+
+        sections.forEach((section) => observer.observe(section))
+
+        // Clean up observer on component unmount
+        return () => observer.disconnect()
+
+    }, [navigate])
+    
+    return null
+}
+
+export default useSectionObserver
