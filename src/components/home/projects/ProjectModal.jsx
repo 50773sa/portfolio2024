aed197ae6d638af5e4d3f62d35a6e2ef02783aee
50773sa <93046590+50773sa@users.noreply.github.com>
2024-11-10 12:46:03 +0100

    chore: Move file

---
+++ src/components/home/projects/ProjectModal.jsx
@@ -0,0 +1,22 @@
+// components
+import ProjectModalContent from './ProjectModalContent'
+// mui
+import Dialog from "@mui/material/Dialog"
+
+const ProjectModal = ({ open, setOpen, project }) => {
+    return (
+        <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            scroll='paper'
+            aria-labelledby="scroll-dialog-title"
+            aria-describedby="scroll-dialog-description"
+            fullWidth={true}
+            PaperProps={{ sx: { maxWidth: '1200px' } }}
+        >
+            <ProjectModalContent open={open} project={project} />
+        </Dialog>
+    )
+}
+
+export default ProjectModal
