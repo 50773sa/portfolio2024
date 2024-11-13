3ecad70a4045b171661a8a055cadbf22a3579aa0
50773sa <93046590+50773sa@users.noreply.github.com>
2024-11-13 18:31:17 +0100

    feat: Add a 'Close'-button to the modal

--- src/components/home/projects/ProjectModal.jsx
+++ src/components/home/projects/ProjectModal.jsx
@@ -1,9 +1,17 @@
 // components
 import ProjectModalContent from './children/ProjectModalContent'
 // mui
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import Dialog from "@mui/material/Dialog"
 
 const ProjectModal = ({ open, setOpen, project }) => {
+
+    const closeModal = (e) => {
+        if (e && e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift') ) {
+            return
+        }
+        setOpen(false)  
+    }
     return (
         <Dialog
             open={open}
@@ -12,8 +20,22 @@ const ProjectModal = ({ open, setOpen, project }) => {
             aria-labelledby="scroll-dialog-title"
             aria-describedby="scroll-dialog-description"
             fullWidth={true}
-            PaperProps={{ sx: { maxWidth: '1200px' } }}
+            PaperProps={{ sx: { maxWidth: '1200px', position: 'relative' } }}
         >
+            <CloseRoundedIcon 
+                role='button'
+                ariaLabel='Close'
+                tabIndex={0}
+                onClick={() => setOpen(false)} 
+                onKeyDown={(e) => closeModal(e)}
+                sx={{ 
+                    position: 'absolute', 
+                    top: 14, 
+                    right: 14, 
+                    cursor: 'pointer',
+                    zIndex: 100000, 
+                }}
+            />
             <ProjectModalContent open={open} project={project} />
         </Dialog>
     )
