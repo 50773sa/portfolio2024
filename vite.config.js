0d2bfad00b04de60932b535a5dafc70c5519d1cf
50773sa <93046590+50773sa@users.noreply.github.com>
2024-11-24 14:42:06 +0100

    Fix: Add the optimizeDeps to prevent error from MUI
    - Prevent the error 'Grid is not a function' from mui that makes the app crash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: { 
+    // prevent the error 'Grid is not a function' from mui
+    include: ['@mui/material/Unstable_Grid2/Grid2'],
+  },
   server: {
     port: 3000,
   }
