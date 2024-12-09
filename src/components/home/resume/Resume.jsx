6d72d10e9eada44330ad40177bf3cc4b911a9aab
50773sa <93046590+50773sa@users.noreply.github.com>
2024-12-09 11:22:31 +0100

    Style: Rearrange the grids

--- src/components/home/resume/Resume.jsx
+++ src/components/home/resume/Resume.jsx
@@ -1,8 +1,11 @@
 // components
 import Education from "./children/Education"
 import Experiences from "./children/Experiences"
+import Interests from "./children/Interests"
 import Languages from "./children/Languages"
 import SectionWrapper from "../SectionWrapper"
+import SoftSkills from "./children/SoftSkills"
+import SoftwareSkills from "./children/SoftwareSkills"
 import TitleWithDescription from "../TitleWithDescription"
 // mui
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
@@ -13,26 +16,28 @@ const Resume = ({ theme, resume, isLoading }) => {
 
         <SectionWrapper bgcolor='dark' sectionId='Resume'>
             
-            <TitleWithDescription
+            <TitleWithDescription 
                 h2='Resume'
-                paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae '
+                paragraph='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia'
             />
 
-            <Grid container spacing={10}>
+            <Grid container justifyContent='center' spacing={10}>
                 {!isLoading && resume && (
                     <>
-                        <Grid xs={12} md={6}>
-                            <Experiences theme={theme} resume={resume} />
+                        <Grid xs={12} md={5}>
+                            <SoftwareSkills resume={resume} />
+                            <Languages resume={resume} theme={theme} />
+                            <SoftSkills resume={resume} theme={theme} />
+                            <Interests resume={resume} theme={theme} />
                         </Grid>
                         <Grid xs={12} md={6}>
-                            <Education theme={theme} resume={resume} />
-                            <Languages theme={theme} resume={resume} />
+                            <Education resume={resume} theme={theme} />
+                            <Experiences resume={resume} theme={theme} />
                         </Grid>
                     </>
                 )}
             </Grid>
         </SectionWrapper>
-        
     )
 }
 
