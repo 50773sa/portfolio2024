7a7cd4cec77d598554ea274c5dc4495bd41c148e
50773sa <93046590+50773sa@users.noreply.github.com>
2024-12-09 14:10:07 +0100

    feat: Show your soft skills

---
+++ src/components/home/resume/children/SoftSkills.jsx
@@ -0,0 +1,38 @@
+// components
+import H2 from "./H2"
+// mui
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import Typography from "@mui/material/Typography"
+
+const SoftSkills = ({ resume, theme }) => {
+
+    return (
+        <>
+            <H2 title='Soft Skills' />
+
+            <Grid container alignItems="center" spacing={1} mb={7}>
+                <Grid xs={12} display="flex" alignItems="center" flexWrap='wrap'>
+                    {resume.map((data) => data.softSkills.map((i) => 
+                        <div
+                            key={i.skill}
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                whiteSpace: "nowrap",
+                            }}
+                        >
+                            <FiberManualRecordIcon sx={{ color: theme.palette.color.orange.medium, fontSize: '8px' }} />
+                            <Typography px={1}>
+                                {i.skill}
+                            </Typography>
+                        </div>
+                    ))}
+                </Grid>
+            </Grid>
+        </>
+    )    
+}
+
+export default SoftSkills
