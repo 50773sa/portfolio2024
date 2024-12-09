4d572daa2eaad0c95a9873684f16900045132be5
50773sa <93046590+50773sa@users.noreply.github.com>
2024-12-09 14:15:25 +0100

    feat: Show your software skills

---
+++ src/components/home/resume/children/SoftwareSkills.jsx
@@ -0,0 +1,31 @@
+// components
+import H2 from "./H2"
+import LinearValueBar from "./LinearValueBar"
+// mui
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import Typography from "@mui/material/Typography"
+
+const SoftwareSkills = ({ resume }) => {
+    return (
+        <>
+            <H2 title='Software Skills' />
+
+            <Grid container alignItems="center" mb={7} spacing={1}>
+                {resume.map((data) =>
+                    data.softwareSkills.map((skill) => (
+                        <>
+                            <Grid xs={6}>
+                                <Typography>{skill.title}</Typography>
+                            </Grid>
+                            <Grid xs={6}>
+                                <LinearValueBar skill={skill} />
+                            </Grid>
+                        </>
+                    ))
+                )}
+            </Grid>
+        </>
+    )
+}
+
+export default SoftwareSkills
