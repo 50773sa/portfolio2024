e65a34fb5f5c81006cd59681009d2784e019ec2e
50773sa <93046590+50773sa@users.noreply.github.com>
2024-12-09 14:32:30 +0100

    style: Add comma between words

--- src/components/home/resume/children/Education.jsx
+++ src/components/home/resume/children/Education.jsx
@@ -2,6 +2,8 @@
 import H2 from "./H2"
 // mui
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
 import Typography from "@mui/material/Typography"
 
 const monthsEN = ["Jan", "Feb", "Mars", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -11,36 +13,46 @@ const Education = ({ resume }) => {
          <>
             <H2 title='Education' />
 
-            <Grid container alignItems="center" mb={7} spacing={1}>
+            <Grid container mb={7} spacing={1}>
                 {resume.map((data) =>
-                    data.educations.map((edu) => {
-                        return (
-                            <Grid xs={12} key={edu.title}>
-                                <Typography sx={{ fontWeight: 600 }}>
-                                    {edu.title}
-                                </Typography>
+                    data.educations.map((edu) => (
+                        <Grid xs={12} key={edu.title}>
+                            <Typography sx={{ fontWeight: 600 }}>
+                                {edu.title}
+                            </Typography>
 
-                                <Typography sx={{ fontSize: 14, fontStyle: "italic" }}>
-                                    {edu.school}
-                                </Typography>
+                            <Typography sx={{ fontSize: 14, fontStyle: "italic" }}>
+                                {edu.school}
+                            </Typography>
 
-                                <Typography mb={2} sx={{ fontSize: 14 }}>
-                                    {edu.city + ' '}
-                                    [
-                                        {monthsEN[edu.period.startMonth - 1]} {edu.period.startYear}
-                                        - 
-                                        {monthsEN[edu.period.endMonth - 1]} {edu.period.endYear}
-                                    ]
-                                </Typography>
+                            <Typography mb={2} sx={{ fontSize: 14 }}>
+                                {edu.city + ' '}
+                                [
+                                    {monthsEN[edu.period.startMonth - 1]} {edu.period.startYear}
+                                    - 
+                                    {monthsEN[edu.period.endMonth - 1]} {edu.period.endYear}
+                                ]
+                            </Typography>
 
-                                <Typography sx={{ fontWeight: 500 }}>
-                                    Technologies
-                                </Typography>
+                            <Typography sx={{ fontWeight: 500 }}>
+                                Technologies
+                            </Typography>
 
-                                <Typography>{edu.technologies}...</Typography>
-                            </Grid>
-                        )
-                    })
+                            <List sx={{ display: 'flex', flexWrap: 'wrap', p: 0 }}>
+                                {edu.technologies.map((technology, i) => (    
+                                    <ListItem key={i} sx={{ width: 'max-content', p: 0, mr: 1  }}>
+                                        <Typography> 
+                                            {technology}
+                                            {/* Comma is only displayed if the current index is not the last index */}
+                                            {i < edu.technologies.length - 1 && ','}
+                                            {/* '...' is only displayed if the current index is the last index */}
+                                            {i == edu.technologies.length - 1 && '...'}
+                                        </Typography>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Grid>
+                    ))
                 )}  
             </Grid>      
         </>
