8d608287c4ae54a292834da5672f64c8047ddbbd
50773sa <93046590+50773sa@users.noreply.github.com>
2025-01-25 12:21:04 +0100

    style: Replace min/maxWidth with aspectRatio 4/4

--- src/components/home/projects/children/LoadingImgSkeletons.jsx
+++ src/components/home/projects/children/LoadingImgSkeletons.jsx
@@ -4,14 +4,15 @@ import Skeleton from '@mui/material/Skeleton'
 
 const LoadingImgSkeletons = () => {
     return (
-        <Grid container spacing={4} justifyContent='center' mb={4} px={2}>
-            {Array(6).fill().map((_, i) => (
+        <Grid container spacing={4} justifyContent='center' mb={4} px={2} width='100%'>
+            {Array(3).fill().map((_, i) => (
                 <Grid xs={12} sm={6} md={4} key={i}>
                     <Skeleton 
                         variant="rectangle" 
                         sx={{ 
-                            minWidth: '200px', 
-                            minHeight: '175px',
+                            width: '100%',
+                            height: '100%',
+                            aspectRatio: 4 / 4,
                         }}
                     />
                 </Grid>
