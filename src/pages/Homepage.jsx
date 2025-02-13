dc53facd302a6739cdb5ef8f3b3654305a233bc9
50773sa <93046590+50773sa@users.noreply.github.com>
2025-02-13 14:12:22 +0100

    other: cleanup

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -33,7 +33,7 @@ const Homepage = () => {
             <Hero theme={theme} />
             
             <div style={{ position: 'sticky', top: '80dvh', zIndex: 100 }}>
-                {<AboutSection theme={theme} about={about} isLoading={isLoadingAbout} />}
+                <AboutSection theme={theme} about={about} isLoading={isLoadingAbout} />
                 <TechStack theme={theme} techStack={technologies} isLoading={isLoadingTechnologies} />
                 <Projects theme={theme} projects={projects} isLoading={isLoadingProjects} />
                 <Resume theme={theme} resume={resume} isLoading={isLoadingResume} />
