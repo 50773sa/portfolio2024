3ca96954f4d62f4a5189b927a003c0026ef45dd2
50773sa <93046590+50773sa@users.noreply.github.com>
2025-02-22 16:48:55 +0100

    style: Add a divider and add h3 as an option

--- src/components/home/TitleWithDescription.jsx
+++ src/components/home/TitleWithDescription.jsx
@@ -1,16 +1,49 @@
 // mui
+import { useTheme } from '@mui/material'
+import Divider from '@mui/material/Divider'
 import Typography from '@mui/material/Typography'
 
-const TitleWithDescription = ({ h2, paragraph }) => {
+const TitleWithDescription = ({ h2, h3, paragraph }) => {
+    const theme = useTheme()
     return (
         <>
-            <Typography component='h2' variant='h4' sx={{ textAlign: 'center', mb: 2, cursor: 'default', zIndex: 10 }}>
-                { h2 }
-            </Typography>
+            {h2 && (
+                <>
+                    <Typography component='h2' variant='h4' sx={{ textAlign: 'start', width: '100%', mb: 2, cursor: 'default', zIndex: 10 }}>
+                        { h2 }
+                    </Typography>
 
-            <Typography component='p' variant='subtitle1' sx={{ textAlign: 'center', mb: 5, cursor: 'default' }}>
-                { paragraph }
-            </Typography>
+                    <div style={{ display: 'flex' }}>
+                        <Divider 
+                            orientation='verticale'
+                            aria-hidden="true" 
+                            sx={{ 
+                                bgcolor: theme.palette.color.orange.light,
+                                height: {xs: '0.2rem', sm: '0.2rem'},
+                                width: {xs: '4rem', sm: '4rem'},
+                                mt: 1.5,
+                                mr: 2
+                            }}
+                        />
+
+                        <Typography component='p' variant='subtitle1' sx={{ textAlign: 'start', mb: 5, cursor: 'default' }}>
+                            { paragraph }
+                        </Typography>
+                    </div>
+                </>
+            )}
+
+            {h3 && (
+                <>
+                    <Typography component='h3' variant='h4' sx={{ textAlign: 'cengter', mb: 2, cursor: 'default', zIndex: 10 }}>
+                        { h3 }
+                    </Typography>
+
+                    <Typography component='p' variant='subtitle1' sx={{ textAlign: 'start', mb: 5, cursor: 'default' }}>
+                        { paragraph }
+                    </Typography>
+                </>
+            )}
         </>
     )
 }
