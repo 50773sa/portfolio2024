30084fc365726e1e765a5250295af3f7646f4bec
50773sa <93046590+50773sa@users.noreply.github.com>
2025-03-13 20:16:48 +0100

    style: add extra containers to control the layout

--- src/components/home/resume/children/Experiences.jsx
+++ src/components/home/resume/children/Experiences.jsx
@@ -1,5 +1,3 @@
-// components
-// import H3 from "./H3"
 // mui
 import Divider from "@mui/material/Divider"
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
@@ -18,97 +16,130 @@ const Experiences = ({ theme, resume }) => {
         marginTop: 8,
         backgroundColor: theme.palette.bgColor.orange,
         boxShadow: '0px 0px 5px 1px' + theme.palette.color.orange.dark,
+        // position: 'relative',
     }
+
+    // const styledBadge = {
+    //     color: theme.palette.color.white,
+    //     backgroundColor: theme.palette.bgColor.light,
+    //     marginTop: 10,
+    //     padding: '10px 30px 10px 10px',
+    //     minWidth: '111px',
+    //     maxWidth: '120px',
+    //     letterSpacing: 0.5,
+    //     fontWeight: 300,
+    //     borderRadius: '2px',
+    //     clipPath: 'polygon(0 0, 75% 0%, 100% 50%, 75% 100%, 0% 100%)',
+    // }
            
     return (
         <>
-            {/* <H3 title='Experiences' /> */}
-            
-            <Grid container alignItems="center" spacing={1}>
-                {resume.map((data) => data.experiences
-                    .sort((a, b) => a.order - b.order)
-                    .map((exp, i) => {
-                        const startMonth = months[exp.period.start.month - 1]
-                        const endMonth =  months[exp.period.end.month - 1]
+            {resume.map((data) => data.experiences
+                .sort((a, b) => a.order - b.order)
+                .map((exp, i) => {
+                    const startMonth = months[exp.period.start.month - 1]
+                    const endMonth =  months[exp.period.end.month - 1]
 
-                        const startYear = exp.period.start.year
-                        const endYear = exp.period.end.year
+                    const startYear = exp.period.start.year
+                    const endYear = exp.period.end.year
 
-                        return (
-                            <Grid xs={12} key={i} display="flex">
-                                <div 
-                                    style={{
-                                        display: "flex", 
+                    return (
+                        <Grid container spacing={1} key={i}>
+
+                            {/* <Grid xs={3}>
+                                <div style={styledBadge}>
+                                    {exp.period.weeks + ' ' + 'weeks'}
+                                </div>
+                            </Grid> */}
+
+                            <Grid // The icon/divider needs extra containers to manage their position
+                                xs={2.2}
+                                sm={1.5}
+                                md={1.2}
+                                sx={{ 
+                                    display: 'flex',
+                                    flexDirection: "column", 
+                                    alignItems: "start", 
+                                }}
+                            >      
+                                <div
+                                    style={{ 
+                                        display: 'flex',
                                         flexDirection: "column", 
-                                        alignItems: "center", 
-                                        marginRight: "16px", 
-                                        marginBottom: '16px',
-                                        paddingBottom: "50px",
-                                        paddingRight: "8px",
+                                        alignItems: "center",
+                                        height: '100%'
                                     }}
                                 >
                                     <div style={styledResumeIcon}>
                                         <WorkIcon />
                                     </div>
-                                    <Divider orientation="vertical" variant="middle" sx={{ /* bgcolor: theme.palette.color.lightGrey  */ width: 2}} aria-hidden="true" /> {/* aria hidden='true' = screen readers will skip this part. is used when the component is only for styling purposes */}
-                                </div>
 
-                                <div>
-                                    {/**
-                                     *  Company
-                                     */}
-
-                                    <Typography component='h4' sx={{ fontWeight: 600 }}>
-                                        {exp.company}
-                                    </Typography>
-
-                                    {/**
-                                     *  Role
-                                     */}
-
-                                    <Typography sx={{ fontSize: 14, fontStyle: "italic" }}>
-                                        {exp.title}
-                                    </Typography>
-
-                                    {/**
-                                     *  City
-                                     */}
-
-                                    <Typography mb={2} sx={{ fontSize: 14 }}>
-                                        {exp.city + " "}[{startMonth}
-                                        {" " + startYear + " "}-{" " + endMonth + " " + endYear}]
-                                    </Typography>
-
-                                    {/**
-                                     *  Case
-                                     */}
-
-                                    <Typography component='h5' sx={{ fontWeight: 500 }}>
-                                        Case
-                                    </Typography>
-
-                                    <Typography paragraph>
-                                        {exp.description}
-                                    </Typography>
-
-                                    {/**
-                                     *  Technologies & tools
-                                     */}
-
-                                    <Typography component='h5' sx={{ fontWeight: 500 }}>
-                                        Technologies & Tools
-                                    </Typography>
-
-                                    <Typography paragraph>
-                                        {exp.technologies}
-                                    </Typography>
-                                </div>
+                                    <Divider 
+                                        orientation="vertical" 
+                                        aria-hidden="true" /* aria hidden='true' = screen readers will skip this part. is used when the component is only for styling purposes */
+                                        variant="middle" 
+                                        sx={{ height: '70%' }}
+                                    /> 
+                                </div>                        
                             </Grid>
-                        )
-                    })
-                )}
-            </Grid>
+
+                            <Grid xs={9}>
+
+                                {/**
+                                 *  Company
+                                 */}
+
+                                <Typography component='h5' sx={{ fontWeight: 600 }}>
+                                    {exp.company}
+                                </Typography>
+
+                                {/**
+                                 *  Role
+                                 */}
+
+                                <Typography sx={{ fontSize: 14, fontStyle: "italic" }}>
+                                    {exp.title}
+                                </Typography>
+
+                                {/**
+                                 *  City
+                                 */}
+
+                                <Typography mb={2} sx={{ fontSize: 14 }}>
+                                    {exp.city + " "}[{startMonth}
+                                    {" " + startYear + " "}-{" " + endMonth + " " + endYear}]
+                                </Typography>
+
+                                {/**
+                                 *  Case
+                                 */}
+
+                                <Typography component='h5' sx={{ fontWeight: 500 }}>
+                                    Case
+                                </Typography>
+
+                                <Typography paragraph>
+                                    {exp.description}
+                                </Typography>
+
+                                {/**
+                                 *  Technologies & tools
+                                 */}
+
+                                <Typography component='h5' sx={{ fontWeight: 500 }}>
+                                    Technologies & Tools
+                                </Typography>
+
+                                <Typography paragraph>
+                                    {exp.technologies}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    )
+                })
+            )}
         </>
+   
     )
 }
 
