09d73df704a9891ec9b18936397c7fc047957761
50773sa <93046590+50773sa@users.noreply.github.com>
2025-03-22 10:57:33 +0100

    style: Apply different styling rules depending on which props are received

--- src/components/home/TitleWithDescription.jsx
+++ src/components/home/TitleWithDescription.jsx
@@ -5,37 +5,51 @@ import Typography from '@mui/material/Typography'
 
 const TitleWithDescription = ({ h2, h3, paragraph }) => {
     const theme = useTheme()
+    const styleMargin = paragraph.length !== 0 ? 2 : 7
+
     return (
         <>
             {h2 && (
                 <>
-                    <Typography component='h2' variant='h4' sx={{ textAlign: 'start', width: '100%', mb: 2, cursor: 'default', zIndex: 10 }}>
+                    <Typography 
+                        component='h2' 
+                        variant='h4' 
+                        sx={{ 
+                            textAlign: 'start', 
+                            width: '100%',
+                            mb: styleMargin, 
+                            cursor: 'default', 
+                            zIndex: 10 
+                        }}
+                    >
                         { h2 }
                     </Typography>
+                    
+                    {h2 == 'About' && (
+                        <div style={{ display: 'flex' }}>
+                            <Divider 
+                                orientation='horizontal'
+                                variant='fullWidth'
+                                aria-hidden="true" 
+                                sx={{
+                                    display: { xs: 'none', md: 'inline-block' }, 
+                                    bgcolor: theme.palette.color.orange.light,
+                                    width: {  xs: '6%' },
+                                    height: 3,
+                                    mt: 1.3,
+                                    mr: 2,
+                                }}
+                            />
 
-                    <div style={{ display: 'flex' }}>
-                        <Divider 
-                            orientation='horizontal'
-                            variant='fullWidth'
-                            aria-hidden="true" 
-                            sx={{
-                                display: { xs: 'none', md: 'inline-block' }, 
-                                bgcolor: theme.palette.color.orange.light,
-                                width: {  xs: '6%' },
-                                height: 3,
-                                mt: 1.3,
-                                mr: 2,
-                            }}
-                        />
-
-                        <Typography 
-                            component='p' 
-                            variant='subtitle1' 
-                            sx={{ textAlign: 'start', mb: 7, cursor: 'default' }}
-                        >
-                            { paragraph }
-                        </Typography>
-                    </div>
+                            <Typography 
+                                component='p' 
+                                variant='subtitle1' 
+                                sx={{ textAlign: 'start', mb: 7, cursor: 'default' }}
+                            >
+                                { paragraph }
+                            </Typography>
+                        </div>
+                    )}
                 </>
             )}
 
